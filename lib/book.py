686476ebaf4553438a10667a250c988326d92175
Aidin <aidin.forooghi@gmail.com>
2023-07-08 08:11:31 -0400

    Passed Tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, new_page_count):
+        if isinstance(new_page_count, int):
+            self._page_count = new_page_count
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self._condition = "Used"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if isinstance(new_size, int):
+            self._size = new_size
+        else:
+            print("size must be an integer")
+
+    @property
+    def condition(self):
+        return self._condition
+
+    def cobble(self):
+        self._condition = "New"
+        print("Your shoe is as good as new!")
+
